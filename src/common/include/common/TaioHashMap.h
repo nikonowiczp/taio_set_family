672d8ce9cdbbf04d234dc83ec9ee9fac61b060a5
Patryk Nikonowicz <pk.nikonowicz@gmail.com>
2022-12-03 20:48:39 +0100

    Add hash map

---
+++ src/common/include/common/TaioHashMap.h
@@ -0,0 +1,15 @@
+#ifndef __TAIOHASHMAP_H__
+#define __TAIOHASHMAP_H__
+
+
+#include "hashmap.h"
+#include "TaioData.h"
+typedef HASHMAP(char, int) HashMap;
+
+
+HashMap InitializeHashMap();
+void FreeHashMap(HashMap map);
+void AddFamily1Set(HashMap map, TaioSet *set);
+void AddFamily2Set(HashMap map, TaioSet* set);
+void PrintContent(HashMap map);
+#endif
---
+++ src/common/src/TaioHashMap.c
@@ -0,0 +1,6 @@
+#include "common/TaioHashMap.h"
+
+HashMap InitializeHashMap();
+void AddFamily1Set(HashMap map, TaioSet *set);
+void AddFamily2Set(HashMap map, TaioSet* set);
+void PrintContent(HashMap map);
