e427a7c0feb0c301bf5dd7f3d52ca55788bf9eed
pasterzewa <ewa.pasterz@gmail.com>
2022-12-04 18:56:13 +0100

    algorytm łatwiejszy zrobiony

--- src/main/main.c
+++ src/main/main.c
@@ -4,9 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int MetricTwo(HashMap* dictionary);
+
+
 int main (int argc, char *argv[]){
     TaioData *parsedData, *reducedData;
     HashMap *map;
+    int metric2;
     if(argc < 2){
         printf("Please provide path to parse set");
         exit(EXIT_FAILURE);
@@ -20,7 +24,30 @@ int main (int argc, char *argv[]){
     reducedData = GetReducedFamilyData(map, parsedData);
     printf("\n\nData after reduction\n\n");
     PrintData(reducedData);
+
+    printf("\n\nEasy algorithm\n\n");
+    metric2 = MetricTwo(map);
+    printf("\n\nCalculated metric = %d\n", metric2);
+
     FreeHashMap(map);
 
     return 0;
+}
+
+int MetricTwo(HashMap* dictionary){
+    int ab = 0, ba = 0;
+    const char *key;
+    int val;
+    printf("Starting metric two\n");
+
+    hashmap_foreach(key, val, dictionary) {
+        if (val > 0){
+            ab += val;
+        }
+        else if (val < 0){
+            ba += (-1)*val;
+        }
+    }
+
+    return ab+ba;
 }
