b22415952f33192fbc58907b9cdfe02a094ebc69
pasterzewa <ewa.pasterz@gmail.com>
2022-12-04 20:20:48 +0100

    main z pytającym loopem

--- src/main/main.c
+++ src/main/main.c
@@ -4,32 +4,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAXPATH 25
+
 int MetricTwo(HashMap* dictionary);
 
 
 int main (int argc, char *argv[]){
     TaioData *parsedData, *reducedData;
     HashMap *map;
-    int metric2;
-    if(argc < 2){
-        printf("Please provide path to parse set");
-        exit(EXIT_FAILURE);
+    int metric = 0, metricValue;
+    char cont = 'Y';
+    char* path =(char*)malloc(sizeof(char)*MAXPATH);
+
+    while(cont != 'N') {
+        printf("Please input path to the text file with data: ");
+        scanf("%s", path);
+
+        printf("Please choose which metric to use (1/2/3): \n");
+        printf("1. Iteration with Jaccard metric.\n");
+        printf("2. Sum of a difference.\n");
+        printf("3. Both.\n");
+        scanf("%d", &metric);
+
+        parsedData = parseData(path);
+        printf("\n\nData read\n\n");
+
+        PrintData(parsedData);
+        map = InitializeHashMap();
+        printf("\n\nReducing data\n\n");
+        reducedData = GetReducedFamilyData(map, parsedData);
+        printf("\n\nData reduced\n\n");
+        PrintData(reducedData);
+
+        switch(metric){
+            case 1:
+                break;
+            case 2:
+                metricValue = MetricTwo(map);
+                printf("\n\nCalculated metric = %d\n", metricValue);
+                break;
+            case 3:
+                printf("Metric as sum of a difference:\n");
+                metricValue = MetricTwo(map);
+                printf("\n\nCalculated metric = %d\n", metricValue);
+                break;
+            default:
+                printf("Sorry, I don't understand.\n");
+        }
+
+        FreeHashMap(map);
+        printf("\n\nWould you like to calculate metrics for another file (Y/N)? ");
+        scanf(" %c", &cont);
     }
-    parsedData = parseData(argv[1]);
-    printf("\n\nData read\n\n");
-
-    PrintData(parsedData);
-    map = InitializeHashMap();
-    printf("\n\nReducing data\n\n");
-    reducedData = GetReducedFamilyData(map, parsedData);
-    printf("\n\nData after reduction\n\n");
-    PrintData(reducedData);
-
-    printf("\n\nEasy algorithm\n\n");
-    metric2 = MetricTwo(map);
-    printf("\n\nCalculated metric = %d\n", metric2);
-
-    FreeHashMap(map);
 
     return 0;
 }
@@ -38,7 +64,6 @@ int MetricTwo(HashMap* dictionary){
     int ab = 0, ba = 0;
     const char *key;
     int val;
-    printf("Starting metric two\n");
 
     hashmap_foreach(key, val, dictionary) {
         if (val > 0){
